6bd81ae86045ab184fe329797ee5458837e99014
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-06-29 15:42:22 +0200

    Added NBT methods to ITileEntityComponent and made it extend INetworkable.

--- src/main/java/org/halvors/electrometrics/common/tileentity/component/ITileEntityComponent.java
+++ src/main/java/org/halvors/electrometrics/common/tileentity/component/ITileEntityComponent.java
@@ -1,7 +1,11 @@
 package org.halvors.electrometrics.common.tileentity.component;
 
+import net.minecraft.nbt.NBTTagCompound;
 import org.halvors.electrometrics.common.component.IComponent;
+import org.halvors.electrometrics.common.tileentity.INetworkable;
 
-public interface ITileEntityComponent extends IComponent {
+public interface ITileEntityComponent extends IComponent, INetworkable {
+    void readFromNBT(NBTTagCompound nbtTags);
 
+    void writeToNBT(NBTTagCompound nbtTags);
 }
